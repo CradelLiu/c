6a4bd0938f26170235b6277d699db98b8f292dca
CradelLiu <*>
2019-02-24 20:12:19 +0800

    *

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "stdio.h"
+#include "stdbool.h"
 #include "math.h"
+
 #include "src/dataStructure/sort.c"
+
 #include "src/basic/chapter4.c"
 #include "src/basic/chapter5.c"
 #include "src/basic/chapter6.c"
 #include "src/basic/chapter7.c"
+
 #include "src/dataStructure/chapter2.c"
 
 int arr[] = {12, 13, 23, 14, 16, 11};
 
 int main() {
-    example_7_16();
+    SqList L;
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+        L.data[i] = arr[i];
+    p18q2(L);
 }
--- src/dataStructure/chapter2.c
+++ src/dataStructure/chapter2.c
@@ -1,13 +1,56 @@
 //线性表
 //静态分配
 #define MaxSize 50
-typedef struct{
-    ElemType data[MaxSize];
+//typedef：为一种数据类型定义一个新名字 以后使用SqList的时候就可以省略struct关键字了
+typedef struct {
+    int data[MaxSize];
     int length;
-}SqList;
+} SqList;
 
 //插入
-bool ListInsert(SqList &L, int i,ElemType e) {
-    if(i<1||i>L.length+1)return false;
+bool ListInsert(SqList L, int i, int e) {
+    if (i < 1 || i > L.length + 1) return false;
+    if (L.length > MaxSize) return false;
+    for (int j = L.length - 1; j >= i; j--)
+        L.data[j + 1] = L.data[j];
+    L.data[i - 1] = e;
+    L.length++;
+    return true;
+}
 
+//删除
+bool ListDelete(SqList L, int i, int e) {
+    if (i < 1 || i > L.length) return false;
+    e = L.data[i - 1];
+    //把第i个元素后面所有的元素赋值给前一个元素
+    for (int j = i; j < L.length; j++)
+        L.data[j - 1] = L.data[j];
+    L.length--;
+}
+
+//按值查找
+int LocateElem(SqList L, int e) {
+    for (int i = 0; i < L.length; i++)
+        if (L.data[i] == e) return i + 1;
+    return 0;
+}
+
+bool p18q1(SqList L, int min) {
+    if (L.length <= 0) {
+        printf("出错");
+        return false;
+    }
+    min = L.data[0];
+    for (int i = 1; i < L.length; i++)
+        if (L.data[i] < min) {
+            min = L.data[i];
+            L.data[i] = L.data[L.length - 1];
+            L.length--;
+        }
+    return true;
+}
+
+void p18q2(SqList L) {
+    for (int i = 0; i < L.length / 2; i++)
+        swap(L.data, i, L.length - 1 - i);
 }
